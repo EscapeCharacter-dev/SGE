93f1084df34b58208a5040b765b09164406880ff
gek <6974902+gordonfreeman424@users.noreply.github.com>
2021-08-06 21:31:50 -0400

    gitap from gek@Katherine.

--- src/internal.h
+++ src/internal.h
@@ -1,7 +1,11 @@
 #ifndef INTERNAL_H
 #define INTERNAL_H
 
+#ifdef __GNUC__
 #define PUBLIC  __attribute__((visibility("default")))
+#else
+#error unsupported compiler
+#endif
 
 void me_free_shaders(void);
 
