9e7214da08c3b57d781e5721ac3852d4e37dad37
EscapeCharacter-dev <30359139+EscapeCharacter-dev@users.noreply.github.com>
2021-08-07 15:09:52 -0400

    New shader function can now free shader/program objects

--- src/shader.c
+++ src/shader.c
@@ -3,8 +3,8 @@
 #include "glad.h"
 #include "cjson.h"
 #include <stdio.h>
-#include <malloc.h>
 #include <string.h>
+#include <stdlib.h>
 
 static uint32_t *objects = 0;
 static uint16_t objects_used = 0;
@@ -32,8 +32,12 @@ static uint32_t appendp(uint32_t val) {
     return programs_used - 1;
 }
 
-/*PUBLIC uint32_t sge_load_shader(const char *json_filepath, uint32_t *out_shader_buffer) {
+PUBLIC uint32_t sge_load_shader(const char *json_filepath, uint32_t *out_shader_buffer, uint32_t *out_shader_count) {
     FILE *fp = fopen(json_filepath, "r");
+    if (!fp) {
+        printf("<ERROR> File %s is not found\n", json_filepath);
+        exit(1);
+    }
     fseek(fp, 0, SEEK_END);
     size_t len = ftell(fp);
     char *buf = malloc(len + 1);
@@ -42,79 +46,72 @@ static uint32_t appendp(uint32_t val) {
     fclose(fp);
     const cJSON *json = cJSON_Parse(buf);
     free(buf);
-    const cJSON *jvert = cJSON_GetObjectItem(json, "vertex");
-    const cJSON *jfrag = cJSON_GetObjectItem(json, "fragment");
-    const cJSON *jattr = cJSON_GetObjectItem(json, "attributes");
-    const char *v_path = jvert->valuestring;
-    const char *f_path = jfrag->valuestring;
-    cJSON *attribute;
-    char **attributes = malloc(sizeof(char **));
-    int i = 0;
-    cJSON_ArrayForEach(attribute, jattr) {
-        char *attr = malloc(strlen(attribute->valuestring));
-        strcpy(attr, attribute->valuestring);
-        attributes[i++] = attr;
-        attributes = realloc(attributes, i * sizeof(char **));
+
+    const cJSON *shaders = cJSON_GetObjectItem(json, "shaders");
+    if (!shaders) {
+        printf("<ERROR> Expected shaders element in shader JSON file\n");
+        exit(1);
     }
-    fp = fopen(v_path, "r");
-    fseek(fp, 0, SEEK_END);
-    len = ftell(fp);
-    buf = malloc(len + 1);
-    fread(buf, 1, len, fp);
-    buf[len] = '\0';
-    uint32_t v = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(v, 1, buf, NULL);
-    glCompileShader(v);
-    fclose(fp);
-    free(buf);
-    int status;
-    static char log[2048];
-    glGetShaderiv(v, GL_COMPILE_STATUS, &status);
-    if (!status) {
-        glGetShaderInfoLog(v, 2048, NULL, log);
-        fprintf(stderr, "Vertex shader compilation error:\n%s\n", log);
-        cJSON_Delete(json);
-        return;
+    cJSON *shader;
+    uint32_t total_shaders = 0;
+    cJSON_ArrayForEach(shader, shaders) {
+        if (total_shaders >= 32) {
+            printf("<WARNING> Max number of shaders reached\n");
+            break;
+        }
+        cJSON *type = cJSON_GetObjectItem(shader, "type");
+        uint32_t shader_type;
+        if (!type) {
+            printf("<ERROR> Shader type is required\n");
+            exit(1);
+        }
+        cJSON *path = cJSON_GetObjectItem(shader, "path");
+        if (!path) {
+            printf("<ERROR> Shader source filepath is required\n");
+            exit(1);
+        }
+        if (!strcmp(type->valuestring, "v")) shader_type = GL_VERTEX_SHADER;
+        else if (!strcmp(type->valuestring, "f")) shader_type = GL_FRAGMENT_SHADER;
+        else if (!strcmp(type->valuestring, "g")) shader_type = GL_GEOMETRY_SHADER;
+        else if (!strcmp(type->valuestring, "c")) shader_type = GL_COMPUTE_SHADER;
+        fp = fopen(path, "r");
+        if (!fp) {
+            printf("<ERROR> Shader source '%s' not found\n", path);
+            exit(1);
+        }
+        fseek(fp, 0, SEEK_END);
+        len = ftell(fp);
+        buf = malloc(len + 1);
+        fread(buf, 1, len, fp);
+        buf[len] = '\0';
+        fclose(fp);
+        uint32_t shader = glCreateShader(shader_type);
+        glShaderSource(shader, len, buf, NULL);
+        glCompileShader(shader);
+        uint32_t status;
+        static char *errorLog[512];
+        glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
+        if (!status) {
+            glGetShaderInfoLog(shader, 512, NULL, errorLog);
+            printf("<ERROR> Shader compilation error:\n%s\n", errorLog);
+            exit(1);
+        }
+        appendo(shader);
+        out_shader_buffer[total_shaders++] = shader;
     }
-    appendo(v);
-    fp = fopen(f_path, "r");
-    fseek(fp, 0, SEEK_END);
-    len = ftell(fp);
-    buf = malloc(len + 1);
-    fread(buf, 1, len, fp);
-    buf[len] = '\0';
-    uint32_t f = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(f, 1, buf, NULL);
-    glCompileShader(f);
-    fclose(fp);
-    free(buf);
-    glGetShaderiv(f, GL_COMPILE_STATUS, &status);
-    if (!status) {
-        glGetShaderInfoLog(f, 2048, NULL, log);
-        fprintf(stderr, "Fragment shader compilation error:\n%s\n", log);
-        cJSON_Delete(json);
-        return;
-    }
-    appendo(f);
     uint32_t program = glCreateProgram();
-    glAttachShader(program, v);
-    glAttachShader(program, f);
+    for (uint32_t i = 0; i < total_shaders; i++) glAttachShader(program, out_shader_buffer[i]);
     glLinkProgram(program);
-    glGetProgramiv(program, GL_LINK_STATUS, &status);
+    uint32_t status;
+    static char *errorLog[512];
+    glGetProgramiv(shader, GL_LINK_STATUS, &status);
     if (!status) {
-        glGetProgramInfoLog(f, 2048, NULL, log);
-        fprintf(stderr, "Shader linkage error:\n%s\n", log);
-        cJSON_Delete(json);
-        return;
+        glGetProgramInfoLog(shader, 512, NULL, errorLog);
+        printf("<ERROR> Shader linkage error:\n%s\n", errorLog);
+        exit(1);
     }
+    *out_shader_count = total_shaders;
     appendp(program);
-    for (int j = 0; j < i; j++) free(attributes[j]);
-    free(attributes);
-    cJSON_Delete(json);
-    if (out_shader_buffer) {
-        out_shader_buffer[0] = v;
-        out_shader_buffer[1] = f;
-    }
     return program;
 }
 
