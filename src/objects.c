123e61fe21c214f02ec726985f3426ce04f6b31e
gek <6974902+gordonfreeman424@users.noreply.github.com>
2021-08-07 14:11:28 -0400

    gitpap from gek@Katherine.

--- src/objects.c
+++ src/objects.c
@@ -18,4 +18,23 @@ void createVBOS(
 		exit(1);
 	}
 	glGenBuffers(NUM_BUFFERS, dest->m_vbos); // Make NUM_BUFFERS buffers
+	for(unsigned int i = 0; i < NUM_BUFFERS; i++){
+		dest->m_vboflags[i] = 0;
+	}
+}
+
+void initArrayBuffer(
+	sge_object_prototype* dest,
+	unsigned int index, //index into the vbo array to initialize
+	unsigned int elems,
+	unsigned int vboflags
+){
+	if(index > SGE_OBJECT_MAX_VBOS){
+		printf("<ERROR> invalid vbo index %u", index);
+		exit(1);
+	}
+	if(dest->m_vbos[index] == 0){
+		printf("<ERROR> cannot initialize null VBO.");
+		exit(1);
+	}
 }
