5135fc05cc2e5ed5d9c096ceafb4435db5c932d8
gek <6974902+gordonfreeman424@users.noreply.github.com>
2021-08-07 14:03:09 -0400

    gitpap from gek@Katherine.

--- src/objects.c
+++ src/objects.c
@@ -1,18 +1,21 @@
 #include "internal.h"
 #include "../lib/glad/glad.h"
 #include "../include/sge.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-
-
-void createVAO(struct sge_object_prototype* dest){
+void createVAO(sge_object_prototype* dest){
 	glGenVertexArrays(1, &dest->m_vao); // allocate a vertex array object
 }
 
 void createVBOS(
-	struct sge_object_prototype* dest,
-	unsigned int 
-
+	sge_object_prototype* dest,
+	unsigned int NUM_BUFFERS /*How many buffers you want?*/
 ){
 	glBindVertexArray(dest->m_vao);
-	glGenBuffers(NUM_BUFFERS, m_vertexArrayBuffers); // Make NUM_BUFFERS buffers
+	if(NUM_BUFFERS > SGE_OBJECT_MAX_VBOS){
+		printf("<ERROR> Too many VBOs. cannot create %u VBOs for VAO.", NUM_BUFFERS);
+		exit(1);
+	}
+	glGenBuffers(NUM_BUFFERS, dest->m_vbos); // Make NUM_BUFFERS buffers
 }
